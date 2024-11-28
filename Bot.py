f74712e20a82ba516e46656783bbed5b597e1cd0
AliMasood1 <ali.masood635@gmail.com>
2024-11-28 20:20:17 +0000

    update

--- Bot.py
+++ Bot.py
@@ -10,13 +10,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 URL =  "https://pump.fun/profile/"
-DEFAULT_TIMEOUT = 35
+DEFAULT_TIMEOUT = 20
 
 
 
 def setup_driver():
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -27,11 +27,10 @@ def getProfile(driver):
     try:
         url = URL + str(input)
         driver.get(url)
-        time.sleep(3) # Wait for the page to load
         WebDriverWait(driver, DEFAULT_TIMEOUT).until(
             ec.presence_of_element_located((By.XPATH, "//*[@id='radix-:r0:']/button"))
         ).click()
-        time.sleep(2)
+
 
         WebDriverWait(driver, DEFAULT_TIMEOUT).until(
             ec.presence_of_element_located((By.XPATH, "//div[contains(text(),'coins created')]"))
